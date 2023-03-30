0684ccca036860135ef3928852dae3a4eb9e9a20
ungyum <simhonfumn58@gmail.com>
2023-03-30 19:32:47 +0900

    add openAI API request

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,24 @@ app.use(cors()); // 나중에 corsOPtions 넣어주기
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// openAI
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// openAI API request
+const apiCall = async () => {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Hello world" }],
+  });
+  console.log(completion.data.choices[0].message); // 이거는 지워도 됨
+  return completion.data.choices[0].message;
+};
+
 // 서버 열기
 app.listen(3000, () => {
   console.log(`Server listening at http://localhost:3000`);
