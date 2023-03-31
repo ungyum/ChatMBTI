fab93721a378d7fd5ade822b654087c503de9859
ungyum <simhonfumn58@gmail.com>
2023-03-31 12:29:40 +0900

    feat: update screens

--- frontend/chat-mbti/src/screens/FirstScreen.tsx
+++ frontend/chat-mbti/src/screens/FirstScreen.tsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
-import Banner from '../components/Banner';
-import Button from '../components/Button';
-import StartButton from '../components/StartButton';
+import React, { useState } from "react";
+import Banner from "../components/Banner";
+import Button from "../components/Button";
+import StartButton from "../components/StartButton";
 
 const FirstScreen: React.FC<{ onNext: () => void }> = ({ onNext }) => {
-  const [selectedButtons, setSelectedButtons] = useState([false, false, false, false]);
+  const [selectedButtons, setSelectedButtons] = useState([
+    false,
+    false,
+    false,
+    false,
+  ]);
 
   const handleClick = (index: number) => {
     const newSelectedButtons = [...selectedButtons];
@@ -18,12 +23,28 @@ const FirstScreen: React.FC<{ onNext: () => void }> = ({ onNext }) => {
 
   return (
     <>
-      <Banner title="ChatMBTI by ChatGPT" />
-      <div style={{ textAlign: 'center' }}>
-        <Button label="I" active={selectedButtons[0]} onClick={() => handleClick(0)} />
-        <Button label="S" active={selectedButtons[1]} onClick={() => handleClick(1)} />
-        <Button label="T" active={selectedButtons[2]} onClick={() => handleClick(2)} />
-        <Button label="J" active={selectedButtons[3]} onClick={() => handleClick(3)} />
+      <Banner />
+      <div style={{ textAlign: "center" }}>
+        <Button
+          label="I"
+          active={selectedButtons[0]}
+          onClick={() => handleClick(0)}
+        />
+        <Button
+          label="S"
+          active={selectedButtons[1]}
+          onClick={() => handleClick(1)}
+        />
+        <Button
+          label="T"
+          active={selectedButtons[2]}
+          onClick={() => handleClick(2)}
+        />
+        <Button
+          label="J"
+          active={selectedButtons[3]}
+          onClick={() => handleClick(3)}
+        />
       </div>
       <StartButton onClick={handleStart} />
     </>
--- frontend/chat-mbti/src/screens/SecondScreen.tsx
+++ frontend/chat-mbti/src/screens/SecondScreen.tsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
-import Banner from '../components/Banner';
+import React, { useState } from "react";
+import Banner from "../components/Banner";
 
 const SecondScreen: React.FC<{ onBack: () => void }> = ({ onBack }) => {
-  const [chatInput, setChatInput] = useState('');
+  const [chatInput, setChatInput] = useState("");
 
-  const handleChatInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChatInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setChatInput(event.target.value);
   };
 
   return (
     <>
-      <Banner title="ChatMBTI by ChatGPT" />
-      <div style={{ textAlign: 'center' }}>
-        <div style={{ margin: '50px' }}>여기에 채팅을 입력하세요:</div>
+      <Banner />
+      <div style={{ textAlign: "center" }}>
+        <div style={{ margin: "50px" }}>여기에 채팅을 입력하세요:</div>
         <input type="text" value={chatInput} onChange={handleChatInputChange} />
       </div>
-      <div style={{ textAlign: 'center' }}>
+      <div style={{ textAlign: "center" }}>
         <button onClick={onBack}>&larr; 돌아가기</button>
       </div>
     </>
