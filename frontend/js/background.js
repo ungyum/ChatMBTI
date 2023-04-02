f87df2d529c9e269ba766dff1cf0232e41c8edf3
ungyum <simhonfumn58@gmail.com>
2023-04-03 00:03:11 +0900

    fix: text-box css

--- frontend/js/background.js
+++ frontend/js/background.js
@@ -1,8 +1,15 @@
 const screen = document.querySelector(".screen");
 const banner = document.querySelector(".banner");
 const ad = document.querySelector(".ad");
+const bannerHeight = banner.offsetHeight;
+const adHeight = ad.offsetHeight;
 let mobileViewHeight = window.innerHeight;
 
-// screen 크기 설정
-screen.style.height =
-  mobileViewHeight - banner.offsetHeight - ad.offsetHeight + "px";
+// 초기에 사이즈 설정해주고
+screen.style.height = mobileViewHeight - bannerHeight - adHeight + "px";
+
+// 창 크기 변경할때마다 다시 설정
+window.addEventListener("resize", () => {
+  mobileViewHeight = window.innerHeight;
+  screen.style.height = mobileViewHeight - bannerHeight - adHeight + "px";
+});
