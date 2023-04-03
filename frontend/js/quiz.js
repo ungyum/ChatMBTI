354fe5da03504348a71dfac48e4f78347dcf9f89
ungyum <simhonfumn58@gmail.com>
2023-04-03 15:06:25 +0900

    fix: remove mbti history

--- frontend/js/quiz.js
+++ frontend/js/quiz.js
@@ -1,11 +1,4 @@
-let mbtiHistory; // mbti 값 기록해줄 변수
-
 quizBtn.addEventListener("click", () => {
-  mbtiHistory = [...mbti]; // 기록해주고
-  // mbti 값 지워주고
-  for (let i = 0; i < 4; i++) {
-    mbti[i] = "?";
-  }
   // 채팅창 지워주고
   chatInterface.innerHTML = "";
 });
