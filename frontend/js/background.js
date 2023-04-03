883f916a0d2648bc944b96be438edd3685ff2800
ungyum <simhonfumn58@gmail.com>
2023-04-03 16:46:23 +0900

    fix: mobile viewport

--- frontend/js/background.js
+++ frontend/js/background.js
@@ -3,12 +3,13 @@ const banner = document.querySelector(".banner");
 const ad = document.querySelector(".ad");
 const bannerHeight = banner.offsetHeight;
 const adHeight = ad.offsetHeight;
+const body = document.querySelector("body");
 
 // 초기에 사이즈 설정해주고
 const adjustScreenHeight = () => {
   screen.style.setProperty(
     "--screen-height",
-    window.innerHeight - bannerHeight - adHeight + "px"
+    body.offsetHeight - bannerHeight - adHeight + "px"
   );
 };
 adjustScreenHeight();
