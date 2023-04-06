27cf54a7d12fc720d3a86b6da1b58c04e7e4eb91
ungyum <simhonfumn58@gmail.com>
2023-04-06 19:01:13 +0900

    feat: add mbti keywords

--- frontend/js/start-screen.js
+++ frontend/js/start-screen.js
@@ -7,25 +7,25 @@ const mbti = ["i", "s", "t", "j"]; // mbti 값 담아줄 변수
 const mainText = document.querySelector(".main-text");
 const mbtiKeywords = {
   // IS** ttff 순, jpjp 순
-  istj: "ISTJ 키워드",
-  istp: "ISTP 키워드",
-  isfj: "ISFJ 키워드",
-  isfp: "ISFP 키워드",
+  istj: "남 얘기 듣는거 안좋아함, 원리원칙, 철벽 잘 침",
+  istp: "만사가 귀찮음, 낯가림, 무미건조",
+  isfj: "배려심 쩔음, 신중함, 열정적",
+  isfp: "귀찮음, 집에 가면 연락 두절됨, 약속 극혐",
   // IN** ttff 순, jpjp 순
-  intj: "INTJ 키워드",
-  intp: "INTP 키워드",
-  infj: "INFJ 키워드",
-  infp: "INFP 키워드",
+  intj: "혼자 있는거 좋아함, 공감 못함, 위로 못함",
+  intp: "자기가 잘난 줄 암, 진지충이라는 소리 들음, 분석 좋아함",
+  infj: "내사람한테는 잘해줌, 생각 많음, 낯가림 심함",
+  infp: "잡생각 많음, 남한테 관심없음, 싫으면 진짜 싫은거",
   // ES** ttff 순, jpjp 순
-  estj: "ESTJ 키워드",
-  estp: "ESTP 키워드",
-  esfj: "ESFJ 키워드",
-  esfp: "ESFP 키워드",
+  estj: "현실적, 상황 분석, 자신에게만 집중",
+  estp: "약간 관종, 눈치 안봄, 리더쉽있음",
+  esfj: "남 눈치 많이 봄, 상담 잘함, 어색한거 못참음",
+  esfp: "성격 급함, 고집 셈, 사교성 개쩜",
   // EN** ttff 순, jpjp 순
-  entj: "ENTJ 키워드",
-  entp: "ENTP 키워드",
-  enfj: "ENFJ 키워드",
-  enfp: "ENFP 키워드",
+  entj: "남일에 노관심, 불도저같은 성격, 공감 못함",
+  entp: "개x마이웨이, 남일에 관심없음, 자신감 충만",
+  enfj: "시끄러움, 잘 맞춰줌, 눈치 빠름",
+  enfp: "감정기복 심함, 친절 베푸는거 좋아함, 호불호 심함",
 };
 
 // 코드줄이기 용
